2894c67dfe8a7c609acfc1f3a323539441be7c45
Yomite47 <olafolarin47@gmail.com>
2026-01-03 17:20:27 +0100

    Remove categories section and cleanup code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import sectorsData from '../data/sectors.json';
 
 export default function Home() {
   const router = useRouter();
